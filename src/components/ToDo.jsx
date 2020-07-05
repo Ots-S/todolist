9b801bdae7e5fe510ef2dcaa90b1b57762fa31df
Seb Ots <ots.sebastien@gmail.com>
2020-07-05 23:10:52 +0200

    feat adding todo

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { Box, Typography } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
 import { makeStyles } from "@material-ui/core/styles";
+
+const WhiteTextTypography = withStyles({
+  root: {
+    color: "#FFFFFF",
+  },
+})(Typography);
 
 const useStyles = makeStyles({
   toDoContainer: {
@@ -8,17 +15,21 @@ const useStyles = makeStyles({
   },
 });
 
-function ToDo() {
+export default function ToDo(props) {
   const classes = useStyles();
+
   return (
     <div>
       <Box className={classes.toDoContainer} mt={2}>
         <Typography variant="h5" align="center">
           TO DO
         </Typography>
+        {props.todos.map((todo) => (
+          <Box p={2} bgcolor="info.main">
+            <WhiteTextTypography key={todo}>{todo}</WhiteTextTypography>
+          </Box>
+        ))}
       </Box>
     </div>
   );
 }
-
-export default ToDo;
